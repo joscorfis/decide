81df8ec8e7eb2d97b2e3bab7a8255707e364ab1a
José María Cornac <56033754+joscorfis@users.noreply.github.com>
2021-02-10 13:43:09 +0100

    Update settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -163,7 +163,7 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
-BASEURL = 'https://egc-app-decide-joscorfis.herokuapp.com'
+BASEURL = 'https://egc-app-decide-pre-joscorfis.herokuapp.com'
 
 try:
     from local_settings import *
